7efb51d2b19efed1e0cfc3495e91dfcc3e2303a8
XinyuuM <dzlenes@163.com>
2024-01-18 00:32:32 +0800

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,124 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Cocomoe',
+  tagline:'万物皆「可萌」(⑅˃◡˂⑅)',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://cocomoe.cn',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'XinyuuM', // Usually your GitHub org/user name.
+  projectName: 'Cocomoedocs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'cn',
+    locales: ['zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/docs/',
+          sidebarPath: './sidebars.js',
+          
+          
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/XinyuuM/Cocomoedocs',
+        },
+        blog:false ,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: '可可萌',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '服务',
+            items: [
+              {
+                label: '博客系统',
+                to: 'https://cocomoe.cn/docs/service',
+              },
+              {
+                label: 'Status',
+                to: 'https://status.cocomoe.cn',
+              },
+            ],
+          },
+          {
+            title: '友情链接',
+            items: [
+              {
+                label: 'Xinyuu的自由天空',
+                to: 'https://xinyuu.cocomoe.cn',
+              },
+            ],
+          },
+          {
+            title: '备案信息',
+            items: [
+              {
+                label: '萌ICP备20242444号',
+                to: 'https://icp.gov.moe/?keyword=20242444',
+              },
+            
+            ],
+          },
+          {
+            
+            
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Cocomoe All rights reserved.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,52 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: '查询文档',
+    Svg: require('@site/static/img/code1.svg').default,
+    description: (
+      <>
+        您可以在此处查询到Cocomoe在近三年编辑过的所有的文档。
+      </>
+    ),
+  },
+  {
+    title: '参与编辑',
+    Svg: require('@site/static/img/code_star.svg').default,
+    description: (
+      <>
+        发现错误？现在您仅需了解Markdown的语法您就可以帮助我们修正文档！
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
