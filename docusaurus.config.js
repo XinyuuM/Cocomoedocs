95ed98a2fa4517ee5d097a42f30980a3b6a1464c
XinyuuM <dzlenes@163.com>
2024-02-05 22:07:37 +0800

    修正逻辑

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,6 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'XinyuuM', // Usually your GitHub org/user name.
@@ -43,7 +42,7 @@ const config = {
         blog: {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          routeBasePath: '/',
+          
           postsPerPage: 4,
           blogSidebarCount: 'ALL',
         },
---
+++ src/pages/index.md
@@ -0,0 +1,13 @@
+
+
+<div style={{textAlign: 'center'}}>
+
+![](/img/link-index.png)
+
+欢迎来到可可萌。
+
+点击[这里](/blog)进入我的Blog
+
+![Dynamic JSON Badge](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fapi.bilibili.com%2Fx%2Frelation%2Fstat%3Fvmid%3D142202292%26jsonp%3Djsonp&query=%24.data.follower&logo=bilibili&label=atxinyuu&color=%2300ccff)
+
+</div>
